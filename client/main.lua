e9ecca58ded35edf18e3429bfada952659a4be5f
Exioo <66903577+ST4LTH@users.noreply.github.com>
2022-07-25 17:16:27 +0200

    fix

--- client/main.lua
+++ client/main.lua
@@ -1,23 +1,5 @@
 local properties = nil
 
-AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then
-        return
-    end
-    Wait(1000)
-    SendNUIMessage({
-        action = "SET_STYLE",
-        data = Config.Style
-    })
-end)
-
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    SendNUIMessage({
-        action = "SET_STYLE",
-        data = Config.Style
-    })
-end)
-
 RegisterNUICallback("buttonSubmit", function(data, cb)
     SetNuiFocus(false)
     properties:resolve(data.data)
@@ -25,14 +7,14 @@ RegisterNUICallback("buttonSubmit", function(data, cb)
     cb("ok")
 end)
 
-RegisterNUICallback("closeMenu", function(_, cb)
+RegisterNUICallback("closeMenu", function(data, cb)
     SetNuiFocus(false)
     properties:resolve(nil)
     properties = nil
     cb("ok")
 end)
 
-local function ShowInput(data)
+function ShowInput(data)
     Wait(150)
     if not data then return end
     if properties then return end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,15 +4,12 @@ game 'gta5'
 description 'QB-Input'
 version '0.0.1'
 
-client_scripts {
-    'client/*.lua'
-}
-
+client_script 'client/main.lua'
 ui_page 'html/index.html'
 
 files {
     'html/index.html',
-    'html/styles/*.css',
+    'html/style.css',
     'html/script.js'
 }
 
